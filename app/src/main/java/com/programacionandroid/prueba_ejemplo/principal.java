f6b53b4319aa70fdfc730c685db0a70d46cf1601
adrian-albanes <fantabulosoadriangmail.com>
2022-09-25 17:38:37 -0300

    Version 1.2

---
+++ app/src/main/java/com/programacionandroid/prueba_ejemplo/principal.java
@@ -0,0 +1,12 @@
+package com.programacionandroid.prueba_ejemplo;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class principal extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
